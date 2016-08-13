1feca3037da8f5ed5efa7e3b560f3bd010c120ce
Riley Trautman <asonix.dev@gmail.com>
2016-08-12 21:22:17 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FormData.Mixfile do
 
   def project do
     [app: :httpoison_form_data,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
